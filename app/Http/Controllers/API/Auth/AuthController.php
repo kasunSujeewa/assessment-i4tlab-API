b94f5b3bc10e1670d79663e0bb7c7e661c20a8fb
Kasun Ranasinghe <kasun1.sujeewa@gmail.com>
2024-09-08 19:36:11 +0530

    added macro respomse

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -3,16 +3,15 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Constants\Constant;
-use App\Http\Controllers\API\BaseAPIController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegistrationRequest;
-use App\Models\User;
 use App\Services\AuthService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
-class AuthController extends BaseAPIController
+class AuthController extends Controller
 {
     protected $authService;
 
@@ -26,7 +25,7 @@ class AuthController extends BaseAPIController
 
         $token = $this->authService->register($request->validated());
 
-        return $this->successResponse($token,Constant::REGISTERED_SUCCESS_MESSAGE,JsonResponse::HTTP_CREATED);
+        return Response::apiSuccess($token,Constant::REGISTERED_SUCCESS_MESSAGE,JsonResponse::HTTP_CREATED);
     }
 
     public function login(UserLoginRequest $request) : JsonResponse
@@ -34,13 +33,13 @@ class AuthController extends BaseAPIController
 
         $token = $this->authService->login($request->validated());
 
-        return $this->successResponse($token,Constant::LOGGED_SUCCESS_MESSAGE);
+        return  Response::apiSuccess($token,Constant::LOGGED_SUCCESS_MESSAGE);
     } 
 
     public function logout(Request $request)
     {
         $request->user('api')->currentAccessToken()->delete();
 
-        return $this->successResponse([],Constant::LOGOUT_SUCCESS_MESSAGE);
+        return  Response::apiSuccess([],Constant::LOGOUT_SUCCESS_MESSAGE);
     }
 }
--- app/Http/Controllers/API/Task/TaskController.php
+++ app/Http/Controllers/API/Task/TaskController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\API\Task;
 
 use App\Constants\Constant;
 use App\Contracts\TaskService;
-use App\Http\Controllers\API\BaseAPIController;
+
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TaskStoringRequest;
 use App\Http\Requests\TaskUpdatingRequest;
-use App\Models\Task;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
-class TaskController extends BaseAPIController
+class TaskController extends Controller
 {
     protected $taskService;
     protected $request_user;
@@ -28,7 +28,7 @@ class TaskController extends BaseAPIController
         
         $tasks = $this->taskService->findAll($this->request_user,$request->get('status'));
 
-        return $this->successResponse($tasks,Constant::TASKS_RECEIVED_SUCCESS_MESSAGE,JsonResponse::HTTP_OK);
+        return Response::apiSuccess($tasks,Constant::TASKS_RECEIVED_SUCCESS_MESSAGE,JsonResponse::HTTP_OK);
     }
 
     /**
@@ -38,7 +38,7 @@ class TaskController extends BaseAPIController
     {
         $task = $this->taskService->store($request->validated(),$this->request_user);
 
-        return $this->successResponse($task,Constant::TASK_CREATED_SUCCESS_MESSAGE,JsonResponse::HTTP_CREATED);
+        return Response::apiSuccess($task,Constant::TASK_CREATED_SUCCESS_MESSAGE,JsonResponse::HTTP_CREATED);
     }
 
     /**
@@ -48,7 +48,7 @@ class TaskController extends BaseAPIController
     {
         $task = $this->taskService->show($id,$this->request_user);
 
-        return $this->successResponse($task,Constant::TASK_RECEIVED_SUCCESS_MESSAGE,JsonResponse::HTTP_OK);
+        return Response::apiSuccess($task,Constant::TASK_RECEIVED_SUCCESS_MESSAGE,JsonResponse::HTTP_OK);
     }
 
     /**
@@ -58,7 +58,7 @@ class TaskController extends BaseAPIController
     {
         $task = $this->taskService->update($id,$request->validated(),$this->request_user);
 
-        return $this->successResponse($task,Constant::TASK_UPDATED_SUCCESS_MESSAGE,JsonResponse::HTTP_OK);
+        return Response::apiSuccess($task,Constant::TASK_UPDATED_SUCCESS_MESSAGE,JsonResponse::HTTP_OK);
     }
 
     /**
@@ -68,6 +68,6 @@ class TaskController extends BaseAPIController
     {
         $this->taskService->delete($id,$this->request_user);
 
-        return $this->successResponse([],Constant::TASK_DELETED_SUCCESS_MESSAGE,JsonResponse::HTTP_OK);
+        return Response::apiSuccess([],Constant::TASK_DELETED_SUCCESS_MESSAGE,JsonResponse::HTTP_OK);
     }
 }
